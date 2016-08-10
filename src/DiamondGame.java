d30051e8484232aa50e125085ba73e67cc118bed
Katoch <rkatoch@paypalcorp.com>
2016-08-10 14:01:37 +0530

    changes to game driver class

--- src/DiamondGame.java
+++ src/DiamondGame.java
@@ -3,94 +3,92 @@ import entities.Deck;
 import pack.PackWithoutJoker;
 
 class DiamondGame {
-	
+
 	private static final int NO_DECKS = 1;
 	private static final Class JOKERPACK = PackWithoutJoker.class;
 	private static final String COMPUTER_SUIT = "SPADE";
 	private static final String PLAYER_SUIT = "HEART";
 	private static final String DISCARD_SUIT = "CLUB";
-	
+
 	private List<Integer> bidOnList;
-	
+
 	Deck gamedeck;
-	
+
 	Player computer;
 	Player player;
-	
+
 	public DiamondGame() throws Exception {
 		initGame();
 	}
-	
+
 	private void initGame() throws Exception {
-		this.gamedeck = new Deck(NO_DECKS,JOKERPACK);
-		
+		this.gamedeck = new Deck(NO_DECKS, JOKERPACK);
+
 		DiamondHand computerHand = new DiamondHand(gamedeck.drawSuit(COMPUTER_SUIT));
 		DiamondHand playerHand = new DiamondHand(gamedeck.drawSuit(PLAYER_SUIT));
-		
-		computer = new Player(computerHand, strategy);
-		player = new Player(computerHand, strategy);
-		
-		//	Discard one suit
+
+		DiamondStrategy computerStrategy = new DiamondStrategy();
+		SimpleStrategy playerStrategy = new SimpleStrategy();
+
+		computer = new Player(computerHand, computerStrategy, "computer");
+		player = new Player(computerHand, playerStrategy, "player");
+
+		// Discard one suit
 		gamedeck.drawSuit(DISCARD_SUIT);
 	}
-	
+
 	private String playGame() {
-		int computerBid,playerBid;
+		int computerBid, playerBid;
 		Card bidOn;
 		int score;
 		int winningBid;
-		while(!gamedeck.isEmpty()) {
+		while (!gamedeck.isEmpty()) {
 			bidOn = gamedeck.draw();
-			
+
 			computerBid = computer.getBid(bidOn).getFaceValue();
 			playerBid = player.getBid(bidOn).getFaceValue();
-			
+
 			score = bidOn.getFaceValue();
 
 			updateScores(computerBid, playerBid, score);
-			
-			computer.updateLastBid(playerBid);
-			player.updateLastBid(computerBid);
-	
+
+			computer.updateBids(playerBid);
+			player.updateBids(computerBid);
+
 		}
-		return getWinner(computer,player);
+		return getWinner(computer, player);
 	}
-	
-	private int updateScores(Integer computerBid, Integer playerBid,Integer score) {
-		
-		if(computerBid==playerBid) {
-			score = score/2;
+
+	private int updateScores(Integer computerBid, Integer playerBid, Integer score) {
+
+		if (computerBid == playerBid) {
+			score = score / 2;
 			computer.updatePoints(score);
 			player.updatePoints(score);
-		}
-		else if(computerBid > playerBid) {			
+		} else if (computerBid > playerBid) {
 			computer.updatePoints(score);
-		}
-		else{
+		} else {
 			player.updatePoints(score);
 		}
 	}
-	
+
 	private String getWinner(Player computer, Player player) {
-		if(computer.getPoints()==player.getPoints()) {
+		if (computer.getPoints() == player.getPoints()) {
 			return "DRAW";
-		}
-		else if(computer.getPoints()>player.getPoints()) {			
+		} else if (computer.getPoints() > player.getPoints()) {
 			return "COMPUTER";
-		}
-		else{
+		} else {
 			return "YOU";
 		}
 	}
 
 	/*
-	 * 	Tester main
+	 * Tester main
 	 */
-	
-	
+
 	public static void main(String[] args) {
 		DiamondGame game = new DiamondGame();
-		System.out.println("RESULT "+game.playGame());
+		System.out.println("RESULT " + game.playGame());
 	}
-	
+
 }
