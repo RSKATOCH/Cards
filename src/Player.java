5061bad0ec3f1eb658633cc7c3797527bf22878d
Dhruv Sethi <dsethi@paypalcorp.com>
2016-08-10 13:57:44 +0530

    changed getBid method to create card

--- src/Player.java
+++ src/Player.java
@@ -5,26 +5,26 @@ public class Player {
 	Integer score = 0;
 	String name;
 	Strategy strategy;
-	
-	public Player(Hand hand,Strategy strategy, String name){
+
+	public Player(Hand hand, Strategy strategy, String name) {
 		this.hand = hand;
 		this.name = name;
 		this.strategy = strategy;
 	}
-	
-	public Card getBid(){
-		return hand.dropCard(strategy.getNextCard());
+
+	public Card getBid() {
+		return hand.dropCard(new Card(String.valueOf(strategy.getNextMove() - 1), hand.cards.get(0).suit));
 	}
-	
-	public Integer getPoints(){
+
+	public Integer getPoints() {
 		return score;
 	}
-	
-	public void updatePoints(Integer score){
+
+	public void updatePoints(Integer score) {
 		this.score += score;
 	}
-	
-	public void updateBids(Integer bid){
+
+	public void updateBids(Integer bid) {
 		this.strategy.updateBids(bid);
 	}
 }
