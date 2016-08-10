f364fd4d84500a74fee991324a535e2b9dd7eb53
Dhruv Sethi <dsethi@paypalcorp.com>
2016-08-10 16:00:01 +0530

    Added countermatching strategy

---
+++ src/CounterMatchingStrategy.java
@@ -0,0 +1,17 @@
+import entities.Card;
+
+public class CounterMatchingStrategy extends Strategy {
+	static String strategy = "23456789TJQKA";
+
+	@Override
+	public String getNextMove(Hand hand, Card bidOn) {
+		return strategy.charAt((Integer.valueOf(bidOn.face) - 1) % strategy.length()) + "";
+	}
+
+	@Override
+	public void updateBids(Integer bid) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
