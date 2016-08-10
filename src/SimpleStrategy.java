7d5e441ee58e56ef7c216c80209a3020227f2060
Katoch <rkatoch@paypalcorp.com>
2016-08-10 15:55:12 +0530

    something

--- src/SimpleStrategy.java
+++ src/SimpleStrategy.java
@@ -18,14 +18,11 @@ public class SimpleStrategy extends Strategy
 		String output = sc.next();
 		Card card = new Card(output,hand.cards.get(0).suit);
 		while(!hand.cards.contains(card)) {
-			
+			System.out.println("Card not in hand, enter again");
 			output = sc.next();
 			card = new Card(output,hand.cards.get(0).suit);
 		}
 		hand.dropCard(card);
-		for(Card c : hand.cards) {
-			System.out.print(c.getFaceValue()+" ");
-		}
 		return output;
 	}
 
